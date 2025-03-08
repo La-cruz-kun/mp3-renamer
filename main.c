d6f45a370ca8b52c0cb878a442ecabe319e6b463
La Cruz <crabraham0@gmail.com>
2025-03-08 17:08:18 +0100

    read files from a directory

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include "parser.c"
 #include <locale.h>
+#include <dirent.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
 #include <string.h>
+
+void details()
+{
+  printf("Artist: %s\n", Artist);
+  printf("Album: %s\n", Album);
+  printf("Title: %s\n", Title);
+}
+
+bool read_dir(char * directory)
+{
+    DIR *dir = opendir(directory);
+    if (dir == NULL) {
+      perror("opening of directory failed");
+      return false;
+    }
+
+    char full_dir[100];
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+      int len;
+      for (len = 0; entry->d_name[len] != '\0'; len++);
+      if (strcmp(entry->d_name + len - 4, ".mp3") == 0) {
+        printf("%s\n", entry->d_name);
+        sprintf(full_dir, "%s%s", directory, entry->d_name);
+        read_id3v2(full_dir);
+        details();
+      }
+    }
+    if (errno != 0) {
+      perror("readdir error");
+    }
+    return true;
+}
+    
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "");
@@ -10,16 +47,9 @@ int main(int argc, char *argv[]) {
     printf("%s\n", argv[1] + i - 1);
     if (strcmp(argv[1] + i - 4, ".mp3") == 0) {
       read_id3v2(argv[1]);
+      details();
     } else if (strcmp(argv[1] + i - 1, "/") == 0) {
-        int needed_byte = snprintf(NULL, 0, "ls %s", argv[1]);
-        char *buffer = malloc(needed_byte);
-        if (!buffer) {
-          printf("buffer not created\n");
-          return 0;
-        }
-
-        snprintf(buffer, needed_byte, "ls %s", argv[1]);
-        system(buffer);
+      read_dir(argv[1]);
     } else {
       printf("Invalid address or file\n");
     }
