028c71b5e477230bc51a5b004e56b9531c06692f
La Cruz <crabraham0@gmail.com>
2025-09-15 13:27:06 +0100

    made the read_id3v2 function rename the mp3 file

--- main.c
+++ main.c
@@ -8,60 +8,61 @@
 
 void details()
 {
-  printf("Artist: %s\n", Artist);
-  printf("Album: %s\n", Album);
-  printf("Title: %s\n", Title);
+    printf("Artist: %s\n", Artist);
+    printf("Album: %s\n", Album);
+    printf("Title: %s\n", Title);
 }
 
 bool read_dir(char * directory)
 {
     DIR *dir = opendir(directory);
     if (dir == NULL) {
-      perror("opening of directory failed");
-      return false;
+        perror("opening of directory failed");
+        return false;
     }
 
     char full_dir[SIZE*3];
     char filename[SIZE*5];
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
-      errno = 0;
-      int len;
-      for (len = 0; entry->d_name[len] != '\0'; len++);
-      if (strcmp(entry->d_name + len - 4, ".mp3") == 0) {
-        sprintf(full_dir, "%s%s", directory, entry->d_name);
-        read_id3v2(full_dir);
-        sprintf(filename, "%s%s - %s.mp3", directory, Artist, Title);
-        if (rename(full_dir, filename) != 0) {
-          perror("unable to rename");
-          return false;
-      }
-      }
+        errno = 0;
+        int len;
+        for (len = 0; entry->d_name[len] != '\0'; len++);
+        if (strcmp(entry->d_name + len - 4, ".mp3") == 0) {
+            sprintf(full_dir, "%s/%s", directory, entry->d_name);
+            read_id3v2(full_dir);
+            /* sprintf(filename, "%s/%s - %s.mp3", directory, Artist, Title); */
+            /* if (rename(full_dir, filename) != 0) { */
+            /*     perror("unable to rename"); */
+            /*     return false; */
+            /* } */
+        }
     }
     if (errno != 0) {
-      perror("readdir error");
+        perror("readdir error");
     }
     closedir(dir);
     return true;
 }
-    
+
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "");
     int i;
     for (i = 0; argv[1][i] != '\0'; i++);
-    printf("%s\n", argv[1] + i - 1);
     if (argc > 2) {
-      printf("too many arguements\n");
-      return 1;
+        printf("too many arguements\n");
+        return 1;
     }
     if (strcmp(argv[1] + i - 4, ".mp3") == 0) {
-      read_id3v2(argv[1]);
-      details();
+        read_id3v2(argv[1]);
+        details();
+    } else if (strcmp(argv[1], ".\n")) {
+        read_dir(argv[1]);
     } else if (strcmp(argv[1] + i - 1, "/") == 0) {
-      read_dir(argv[1]);
+        read_dir(argv[1]);
     } else {
-      printf("Invalid address or file\n");
+        printf("Invalid address or file\n");
     }
     //printf("Title: %s\n", Title);
     return 0;
--- parser.c
+++ parser.c
@@ -19,16 +19,16 @@ unsigned int syncsafe_to_int(unsigned char *bytes) {
 
 void sanitize_filename(char * filename)
 {
-  char *src = filename;
-  char *dst = filename;
-  while (*src) {
-    if (*src == '/' || *src == '\0') {
-      src++;
-      continue;
+    char *src = filename;
+    char *dst = filename;
+    while (*src) {
+        if (*src == '/' || *src == '\0') {
+            src++;
+            continue;
+        }
+        *dst++ = *src++;
     }
-    *dst++ = *src++;
-  }
-  *dst = '\0';
+    *dst = '\0';
 }
 
 void r_file()
@@ -37,62 +37,63 @@ void r_file()
     TEMP[sizeof(TEMP) - 1] = '\0';
     sanitize_filename((char *)TEMP);
     if (strlen((char *)TEMP) == 0) {
-      printf("%s\n", Title);
-      fprintf(stderr, "filename invalid after sanitization\n");
-      return;
+        printf("%s\n", Title);
+        fprintf(stderr, "filename invalid after sanitization\n");
+        return;
     }
 }
 
 void read_frame(FILE *file, char *frame_id, unsigned int tag_size) {
     unsigned int frame_start = ftell(file);
     while (ftell(file) < tag_size + HEADER_SIZE) {
-      char frame_header[10];
-      fread(frame_header, 1, 10, file);
+        char frame_header[10];
+        fread(frame_header, 1, 10, file);
 
-      if (strncmp(frame_header, frame_id, 4) == 0) {
-          unsigned int frame_size = syncsafe_to_int((unsigned char *)frame_header + 4);
+        if (strncmp(frame_header, frame_id, 4) == 0) {
+            unsigned int frame_size = syncsafe_to_int((unsigned char *)frame_header + 4);
 
-          unsigned char *data = malloc(frame_size + 1);
-          fread(data, 1, frame_size, file);
-          data[frame_size] = '\0';
+            unsigned char *data = malloc(frame_size + 1);
+            fread(data, 1, frame_size, file);
+            data[frame_size] = '\0';
 
-          // Skip encoding byte
-          if (data[0] == 0x03) {
-            memcpy(Value, data + 1, (frame_size + 1));
-            fseek(file, frame_start, SEEK_SET);
-            return;
-          }else if (data[0] == 0x01) {
-            // use iconv header to parser utf 16
-            iconv_t cd = iconv_open("UTF-8", "UTF-16LE"); // define the conversion
-            size_t in_bytes = frame_size; // the size of the input
-            char *in_buf = (char *)&data[3]; // the input itself starts at index 3 because we skip both the encoding info and the BOM
-            char out_buf[100];
-            char *out_ptr = out_buf;
-            size_t out_bytes = sizeof(out_buf);
+            // Skip encoding byte
+            if (data[0] == 0x03) {
+                memcpy(Value, data + 1, (frame_size + 1));
+                fseek(file, frame_start, SEEK_SET);
+                return;
+            } else if (data[0] == 0x01) {
+                // use iconv header to parser utf 16
+                iconv_t cd = iconv_open("UTF-8", "UTF-16LE"); // define the conversion
+                size_t in_bytes = frame_size; // the size of the input
+                char *in_buf = (char *)&data[3]; // the input itself starts at index 3 because we skip both the encoding info and the BOM
+                char out_buf[100];
+                char *out_ptr = out_buf;
+                size_t out_bytes = sizeof(out_buf);
 
-            iconv(cd, &in_buf, &in_bytes, &out_ptr, &out_bytes); // do the conversion
-            iconv_close(cd);
-            memcpy(Value, out_buf, out_bytes);
-            fseek(file, frame_start, SEEK_SET);
-            return;
-          } else {
-            memcpy(Value, data + 1, (frame_size + 1));
-            fseek(file, frame_start, SEEK_SET);
-            return;
-          }
+                iconv(cd, &in_buf, &in_bytes, &out_ptr, &out_bytes); // do the conversion
+                iconv_close(cd);
+                memcpy(Value, out_buf, out_bytes);
+                fseek(file, frame_start, SEEK_SET);
+                return;
+            } else {
+                memcpy(Value, data + 1, (frame_size + 1));
+                fseek(file, frame_start, SEEK_SET);
+                return;
+            }
 
-          free(data);
-      } else {
-          // Seek to next frame
-          unsigned int frame_size = syncsafe_to_int((unsigned char *)frame_header + 4);
-          fseek(file, frame_size, SEEK_CUR);
-      }
+            free(data);
+        } else {
+            // Seek to next frame
+            unsigned int frame_size = syncsafe_to_int((unsigned char *)frame_header + 4);
+            fseek(file, frame_size, SEEK_CUR);
+        }
     }
     fseek(file, frame_start, SEEK_SET);
     Value[0] = '\0';
 }
 
 void read_id3v2(const char *filename) {
+    printf("%s", filename);
     FILE *file = fopen(filename, "rb");
     if (!file) {
         perror("File open error");
@@ -119,6 +120,11 @@ void read_id3v2(const char *filename) {
     read_frame(file, "TPE1", tag_size);  // Artist
     r_file();
     memcpy(Artist, TEMP, sizeof(TEMP));
+    char newname[256] = {0};
+    sprintf(newname, "%s - %s", Artist, Title);
+
+    rename(filename,  newname);
+
 
     fclose(file);
 }
